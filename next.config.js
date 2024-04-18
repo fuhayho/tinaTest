2a8de2110fe479f42598bdfb1fe388bb1660e785
fuhayho <107245153+fuhayho@users.noreply.github.com>
2024-04-18 16:16:35 +0700

    testing

--- next.config.js
+++ next.config.js
@@ -20,4 +20,12 @@ module.exports = {
       },
     ];
   },
+}
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  basePath: "/2048-in-react",
+  output: "export",
+  reactStrictMode: true,
 };
+
+module.exports = nextConfig;;
